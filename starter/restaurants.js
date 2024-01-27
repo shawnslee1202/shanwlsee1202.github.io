b6496ba618265cf9c923c5933bf1deb3ae9e8eb5
ashleeajala <ashlee_ajala04@tamu.edu>
2024-01-27 17:45:48 -0600

    Update restaurants.js

--- starter/restaurants.js
+++ starter/restaurants.js
@@ -3,5 +3,35 @@ var restaurants = [
         "Type": "Japanese",
         "Name": "Saraka Ramen",
         "Rating": 4.8
+    },
+    {
+        "Type": "Mexican"
+        "Name": "Los Cucos Mexican Cafe"
+        "Rating" : 3.9
+    },
+    {
+        "Type": "Mexican"
+        "Name": "Coco Loco"
+        "Rating": 4.3
+    },
+    {
+        "Type": "Mexican"
+        "Name": "The Tacobar"
+        "Rating": 4.5
+    },
+    {
+        "Type": "Korean"
+        "Name": "Choi's Restaurant"
+        "Rating": 4.4
+    },
+    {
+        "Type":"Korean"
+        "Name": "Mong Chon Grill"
+        "Rating": 4.5
+    },
+    {
+        "Type": "Korean"
+        "Name": "Ohana Korean Grill"
+        "Rating": 4.6
     }
 ]
