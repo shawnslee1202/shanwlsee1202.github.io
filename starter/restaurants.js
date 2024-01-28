cb02df0ae6180724ddb79288ef8055063ce9cd6e
ashleeajala <ashlee_ajala04@tamu.edu>
2024-01-27 18:09:03 -0600

    Update restaurants.js

--- starter/restaurants.js
+++ starter/restaurants.js
@@ -5,33 +5,56 @@ var restaurants = [
         "Rating": 4.8
     },
     {
-        "Type": "Mexican"
-        "Name": "Los Cucos Mexican Cafe"
+        "Type": "Mexican",
+        "Name": "Los Cucos Mexican Cafe",
         "Rating" : 3.9
     },
     {
-        "Type": "Mexican"
-        "Name": "Coco Loco"
+        "Type": "Mexican",
+        "Name": "Coco Loco",
         "Rating": 4.3
     },
     {
-        "Type": "Mexican"
-        "Name": "The Tacobar"
+        "Type": "Mexican",
+        "Name": "The Tacobar",
         "Rating": 4.5
     },
     {
-        "Type": "Korean"
-        "Name": "Choi's Restaurant"
+        "Type": "Korean",
+        "Name": "Choi's Restaurant",
         "Rating": 4.4
     },
     {
-        "Type":"Korean"
-        "Name": "Mong Chon Grill"
+        "Type":"Korean",
+        "Name": "Mong Chon Grill",
         "Rating": 4.5
     },
     {
-        "Type": "Korean"
-        "Name": "Ohana Korean Grill"
+        "Type": "Korean",
+        "Name": "Ohana Korean Grill",
         "Rating": 4.6
+    },
+    {
+        "Type": "American",
+        "Name": "Willie's Grill & Icehouse",
+        "Rating": 4.5
+    },
+    {
+        "Type": "Indian",
+        "Name": "TAZ Indian Cuisine",
+        "Rating": 4.2
+    },
+    {
+        "Type": "Italian",
+        "Name": "Casa Mangiare",
+        "Rating": 4.6
+    },
+    {
+        "Type": "Thai",
+        "Name": "Siam Iyara",
+        "Rating": 4.8
+    },
+    {
+        
     }
 ]
